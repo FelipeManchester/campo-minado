ff3cd1f7fa330143c49f4bc52ad3c11ea3a41b52
Felipe Manchester <f.p2809@gmail.com>
2022-08-13 18:26:19 -0300

    feat: add javascript

---
+++ script.js
@@ -0,0 +1,50 @@
+let components = {
+  num_of_row: 12,
+  num_of_cols: 24,
+  num_of_bombs: 55,
+  bomb: '💣',
+  alive: true,
+  colors: {
+    1: 'blue',
+    2: 'green',
+    3: 'red',
+    4: 'purple',
+    5: 'maroon',
+    6: 'turquoise',
+    7: 'black',
+    8: 'grey'
+  }
+};
+
+window.addEventListener('load', function () {
+  document.getElementById('lost').style.display = 'none';
+  startGame();
+});
+
+function startGame() {
+  components.bombs = placeBombs();
+  document.getElementById('field').appendChild(createTable());
+}
+
+function cellID(i, j) {
+  return 'cell-' + i + '-' + j;
+}
+
+function createTable() {
+  let table, row, td, i, j;
+  table = document.createElement('table');
+
+  for (i = 0; i < components.num_of_row; i++) {
+    row = document.createElement('tr');
+    for (j = 0; j < components.num_of_cols; j++) {
+      td = document.createElement('td');
+      td = document.createElement('td');
+      td.id = cellID(i, j);
+      row.appendChild(td);
+      row.appendChild(td);
+      addCellListeners(td, i, j);
+    }
+    table.appendChild(row);
+  }
+  return table;
+}
